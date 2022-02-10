8b13b9166ed6675770654091b2b9f010143c5e9c
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-02-10 22:53:47 +0400

    Second commit

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def decorator(path):
             _datetime = datetime.datetime.today().strftime(f'date: %d.%m.%Y, time: %H:%M:%S')
             file_name = f'function name: {external.__name__}'
             inputs = f'inputs = {args}, {kwargs}'
-            result = f'result = {external(*args, **kwargs)}'
-            function_details = [_datetime, file_name, inputs, result]
-            with open(os.path.join(path, f'{external.__name__}.txt'), 'a', encoding='utf-8') as f:
+            result = external(*args, **kwargs)
+            function_details = [_datetime, file_name, inputs, f'result = {result}']
+            with open(os.path.join(path, f'{external.__name__}.txt'), 'w', encoding='utf-8') as f:
                 data = '\n'.join(str(item) for item in function_details)
                 f.write(data + '\n')
             return result
@@ -30,3 +30,19 @@ def foo2(a, b):
 
 foo(5, 6)
 foo2(a=10, b=33)
+
+
+@decorator(os.getcwd())
+def summator(x, y):
+    return x + y
+
+
+three = summator(1, 2)
+five = summator(2, 3)
+
+res = summator(three, five)
+
+print('result: ', res)
+print('result type: ', type(res))
+
+
